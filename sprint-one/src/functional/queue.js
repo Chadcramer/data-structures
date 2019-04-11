57e0e83f8513590fb0128958efc5ed3830fe5431
Chadcramer <chadpcramer@gmail.com>
2019-04-10 20:09:00 -0500

    finished que() functional instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,19 +3,54 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  
 
   // Implement the methods below
+  var latestKey = 0;
 
   someInstance.enqueue = function(value) {
+  // Add a string to the back of the queue
+  // {0: '1st value'}
+  // {0: '1st value', 1: '2nd value'}
+  
+  someInstance[latestKey] = value;
+  latestKey++;
+ 
   };
 
   someInstance.dequeue = function() {
+  // Remove and return the string at the front of the queue
+
+    var incrementer =0;
+    var returnValue = someInstance['0']; 
+    while(incrementer < latestKey){
+        someInstance[incrementer] = someInstance[incrementer+1];
+        incrementer++;
+    }
+    delete someInstance[latestKey-1];
+    latestKey--;
+    return returnValue;
+  
   };
 
   someInstance.size = function() {
+  // Return the number of items in the queue
+   return latestKey;
   };
-
   return someInstance;
 
 //test
 };
+
+// var test = Queue();
+// test.enqueue('1st value');
+// console.log(test);
+// test.enqueue('2nd value');
+// console.log(test);
+// test.enqueue('3rd value');
+// console.log(test);
+// test.dequeue();
+// console.log(test);
+// test.size();
+
+
