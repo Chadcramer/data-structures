1cb52b103cb51623af4ccf33d80053c2c5a02ac8
Chadcramer <chadpcramer@gmail.com>
2019-04-12 11:39:41 -0500

    progress!

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  //children is an array;
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,6 +14,9 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+
+
+
 };
 
 treeMethods.contains = function(target) {
@@ -21,3 +27,19 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+//  var root = Tree(0);
+
+//  root.addChild(1);
+//  root.addChild(2);
+
+//  root.children[0] //child1 object
+//  root.children[1] //child2 object
+//  root.children[0].addChild(3);
+
+
+
+
+ //root
+//  {value: 0, children:[{value:1, children: [{value:3, children: null}]}, {value:2, children: null}]}
