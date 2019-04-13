8374a7aa1a042a982daa7b47903f642c554f0c8c
Chadcramer <chadpcramer@gmail.com>
2019-04-13 11:06:32 -0500

    graph implementation, all tests passing.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -7,50 +7,72 @@ var Graph = function() {
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+Graph.prototype.addNode = function(input) {
 // method that takes a new node and adds it to the graph
-    var vertice = {'value': null};
-    vertice['value'] = node;
-    this.node = vertice;
+    this[input] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
+Graph.prototype.contains = function(input) {
 // method that takes any node and returns a boolean reflecting whether it can be found in the graph
-    return (this.node) ? true : false;
+    return (this.hasOwnProperty(input));
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
+Graph.prototype.removeNode = function(input) {
 // method that takes any node and removes it from the graph, if present. All edges connected to that Node are removed as well.
-    delete this.node;
+    delete this[input];
+   
     for(var key in this){
-        delete this[key][node];
+        if(this.key){
+        this[key].splice(this.key.indexOf(input), 1);
+        }
     }
+
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
 // method that returns a boolean reflecting whether or not two nodes are connected
+ if(this[fromNode] && this[toNode]){
+
+     if (this[fromNode].includes(toNode) && this[toNode].includes(fromNode)){
+         return true;
+     }
+ }
+ return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
 // method that creates a edge (connection) between two nodes if they both are present within the graph
+  fromNode = Number(fromNode);
+  toNode = Number(toNode);
+    
+  if(this[fromNode] && this[toNode]){
+    this[fromNode].push(toNode);
+    this[toNode].push(fromNode);
+  }
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
 // method that removes the connection between two nodes
+  if(this[fromNode]){
+    this[fromNode].splice(this[fromNode].indexOf(toNode),1);
+  } 
+  if(this[toNode]){
+    this[toNode].splice(this[toNode].indexOf(fromNode),1); 
+  }
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
 // method that traverses through the graph, calling a passed in function once on each node
+
+    for(var key in this){
+        cb(key);
+    }
+
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-
